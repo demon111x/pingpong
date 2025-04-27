9446946e98ee35618bf51d0fee49b74929a78eb7
demon111x <marhaluka@gmail.com>
2025-04-27 23:43:08 +0300

    beta test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 
 from random import randint
+import time
 
 class Base_sprite(pg.sprite.Sprite):
     def __init__(self, pic, x, y, w, h, speed_x=0, speed_y=0):
@@ -25,26 +26,103 @@ class Base_sprite(pg.sprite.Sprite):
         self.rect.y += self.speed_y
 
 
-
-class Player(Base_sprite):
+class Player_left(Base_sprite):
     def update(self):
         keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT] and self.rect.x >= 5:
-            self.rect.x -= self.speed_x
+        # if keys[pg.K_a] and self.rect.x >= 5:
+        #     self.rect.x -= self.speed_x
             
-        if keys[pg.K_RIGHT] and self.rect.x <= win_w - self.rect.width:            
-            self.rect.x += self.speed_x 
+        # if keys[pg.K_d] and self.rect.x <= win_w - self.rect.width:            
+        #     self.rect.x += self.speed_x 
+            
+        if keys[pg.K_w] and self.rect.y >= 5:
+            self.rect.y -= self.speed_y 
+              
+        if keys[pg.K_s] and self.rect.y <= win_h - self.rect.height:            
+            self.rect.y += self.speed_y
+
+
+class Player_right(Base_sprite):
+    def update(self):
+        keys = pg.key.get_pressed()
+        # if keys[pg.K_LEFT] and self.rect.x >= 5:
+        #     self.rect.x -= self.speed_x
+            
+        # if keys[pg.K_RIGHT] and self.rect.x <= win_w - self.rect.width:            
+        #     self.rect.x += self.speed_x 
             
         if keys[pg.K_UP] and self.rect.y >= 5:
             self.rect.y -= self.speed_y 
               
         if keys[pg.K_DOWN] and self.rect.y <= win_h - self.rect.height:            
             self.rect.y += self.speed_y
+
+# class Ball(Base_sprite):
+#     def update(self):
+#         if self.rect.x <= 0 or self.rect.x >= win_w - self.rect.width:
+#             self.speed_x *= -1
+
+#         if self.rect.y <= 0 or self.rect.y >= win_h - self.rect.height:
+#             self.speed_y *= -1
+
+#         if self.rect.x <= win_w and self.rect.x >= 0:
+#             self.rect.x += self.speed_x
+
+#         if self.rect.y <= win_h and self.rect.y >= 0:
+#             self.rect.y += self.speed_y
         
+        
+
+class Ball(Base_sprite):
+    def update(self):
+        global right_missed
+        global left_missed
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.x < 0:
+            ball_bouncing.play()
+            self.speed_x *= -1
+            self.rect.x = 0  
+
+        if self.rect.x + self.rect.width > win_w:
+            ball_bouncing.play()
+            self.speed_x *= -1
+            self.rect.x = win_w - self.rect.width 
+
+        if self.rect.y < 0:
+            ball_bouncing.play()
+            self.speed_y *= -1
+            self.rect.y = 0
+        
+        if self.rect.y + self.rect.height > win_h:
+            ball_bouncing.play()
+            self.speed_y *= -1
+            self.rect.y = win_h - self.rect.height
+
+        if self.rect.x <= 0:
+            left_missed += 1
+            self.rect.x = 725
+            self.rect.y = 375
+            self.speed_x = 7
+            self.speed_y = 7
+
+        if self.rect.x >= win_w - self.rect.width:
+            right_missed += 1
+            self.rect.x = 725
+            self.rect.y = 375
+            self.speed_x = 7
+            self.speed_y = 7
+            self.speed_x *= -1
+            self.speed_y *= -1
+            
+            
+
+
 
 
 pg.font.init()
-font1 = pg.font.Font(None, 36)
+font1 = pg.font.Font(None, 100)
 # font2 = font.Font(None, 20)
 
 win_w = 1500
@@ -57,7 +135,12 @@ mw = pg.display.set_mode(win_size)
 pg.display.set_caption("Ping Pong")
 clock = pg.time.Clock()
 
+pg.mixer.init()
+#pg.mixer.music.play()
+ball_bouncing = pg.mixer.Sound('sounds\\0427.ogg')
+
 players = pg.sprite.Group()
+balls = pg.sprite.Group()
 all_sprite = pg.sprite.Group()
 
 # fon = pg.transform.scale(
@@ -70,15 +153,19 @@ play = True
 win = False
 game = True
 ticks = 1
-ufo_missed = 0
+left_missed = 0
+right_missed = 0
 BLUE = (0, 114, 255)
 
-player_left = Player('images\ping_rocket1.png', win_w*0.07, (win_h-220)/2, 50, 220, 7, 7)
-player_right = Player('images\ping_rocket2.png', win_w*0.9, (win_h-220)/2, 50, 220, 7, 7)
-ball = Player('images\\ball2.png', (win_w-50)/2, (win_h-50)/2, 50, 50, 7, 7)
+player_left = Player_left('images\ping_rocket1.png', win_w*0.07, (win_h-220)/2, 40, 220, 7, 7)
+player_right = Player_right('images\ping_rocket2.png', win_w*0.9, (win_h-220)/2, 40, 220, 7, 7)
+ball = Ball('images\\ball2.png', (win_w-50)/2, (win_h-50)/2, 50, 50, 7, 7)
 all_sprite.add(player_left)
 all_sprite.add(player_right)
 all_sprite.add(ball)
+players.add(player_left)
+players.add(player_right)
+balls.add(ball)
 
 def set_text(text, x, y, color=(255,255,200)):
     mw.blit(
@@ -87,7 +174,12 @@ def set_text(text, x, y, color=(255,255,200)):
 
 
 while play:
-
+    if ticks % 100 == 0:
+        if ball.speed_x >= 150:
+            ball.speed_x = 7
+        else:
+            ball.speed_x *= 1.1
+            ball.speed_y *= 1.1
     for e in pg.event.get():
         if e.type == pg.QUIT or \
                     (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
@@ -98,16 +190,19 @@ while play:
         all_sprite.update()      
         all_sprite.draw(mw)      
         
+        if pg.sprite.groupcollide(players, balls, False, False):
+            ball_bouncing.play()
+            ball.speed_x *= -1
+            ball.speed_y *= -1
        
  
     
-        set_text(f"Пропущено: ", 650, 20)
+        set_text(f"{right_missed}:{left_missed}", win_w/2-50, 20)
 
        
 
     else:
         # mw.blit(fon_go, (0, 0))
-        (0, 114, 255)
         mw.fill(BLUE)
 
     
